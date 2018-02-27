53dc3698d7903e399f76de80db89aee3d914fa6a
Jonathan Greene <jonsgreene@gmail.com>
2018-02-26 21:54:24 -0500

    Added model validation stopped at 6.2.4

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test 'name and email should be present' do
+    @user.name = "                     "
+    @user.email = "                    "
+    assert_not @user.valid?
+  end
+
+  test 'name and email should not be too long' do
+    @user.name = "a" * 51
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
 end

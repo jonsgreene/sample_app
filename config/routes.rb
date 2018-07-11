0dd125c20299768a4619b463281de90da82ea8f7
Jonathan Greene <jonsgreene@gmail.com>
2018-07-11 14:09:42 -0400

    Fixed failing test redirect user after signup to users page added route for create Stopped at 7.4.2

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+  
+  post '/signup', to: 'users#create'
+
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

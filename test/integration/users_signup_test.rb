34cadb6cc4e99d004e9871f5387835dc46611f54
Jonathan Greene <jonathan_greene1@homedepot.com>
2018-12-09 17:35:08 -0500

    Update Readme
    Updated readme with current book location

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",

4135656f9a4d68e316bf356052f190322fd61497
Jonathan Greene <jonathan_greene1@homedepot.com>
2019-01-15 13:14:54 -0500

    Update sessions controller
    Add create methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in and go to the users show page
+    else
+      # create error message
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end

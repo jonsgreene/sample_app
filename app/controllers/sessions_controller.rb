d541a316f4e504fc1c3dbf5d3520d306428a1efa
Jonathan Greene <jonathan_greene1@homedepot.com>
2019-01-26 10:10:35 -0500

    Login
    Add  ability to login
    Add dropdown to show specific menu for users who are loggged in
    stopped at 8.2.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # log in and go to the users show page
+      log_in user
+      redirect_to user
     else
-      # create error message
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
